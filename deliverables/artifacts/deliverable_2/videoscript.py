1c832f5f87c61103612e61ce144199f4945df40d
kabir301 <joejonas9921@gmail.com>
2017-03-06 15:23:46 -0800

    Committed video script to deliverable 2

---
+++ deliverables/artifacts/deliverable_2/videoscript.py
@@ -0,0 +1,38 @@
+Script
+
+Scene 1: 
+
+A person (with carpal tunnel syndrome) enters the room and attempts to use the keyboard on his phone.
+As he is making gestures with his finger, it becomes clear he suffers from pain whenever he makes certain gestures with his hands. He gives up, he does a search on the internet for 'alternate accessible keyboards', and the first link is MotionKey. He is curious, and he installs it.
+
+Scene 2:
+
+Hi, we are group 15. Our product is MotionKey - a keyboard that is entirely controlled through sensors in SmartPhones rather
+than via the touchscreen. This product will augment the ability of marginalized groups to better control the user interface.
+Whether you are a war veteran who lost his limbs in Iraq or a person who suffers from carpal tunnel syndrome and so cannot
+use a touchscreen without suffering from hand pain, our product is aimed to improve your user experience. You simply install the product,
+and every time the softwar keyboard on android is supposed to appear, our keyboard appears instead.
+
+Here, we show a demonstration of our product:
+ *someone starts off by sideloading the app to their phone*
+ *then someone switches the regular keyboard to this keyboard*
+ *then start up the browser, and click on the search bar, instead of the usual keyboard appearing, this one will apear*
+ *then, demonstrate typing www.google.com on the keyboard*
+ *then once google starts up again, we enter something into the google search bar*
+
+Scene 3:
+
+ Now, go back to talking:
+ As you can see, the entire work flow of using your telephone will eliminate having to ever to use the touch keyboard.
+ All key presses can be registered solely through physical movements of the keyboard itself, which opens up a vast number
+ of possibilities for individuals who may not have physically be capable of using the touchscreen. While our team specifically
+ is focusing on making the keyboard, we are abstracting away the job of how a person can interact with
+ the screen for other activities like selection icons to a different team, so that the entire user experience of using 
+ a modern smartphone can eliminate touch gestures.
+
+ Scene 4:
+
+ For our backlog we are planning to streamline the user experience of using the keyboard. Keys will be larger and more visually appearling - the general feeling of the motion will be smoother and more user friendly. We also plan to make a 'suite' of apps that can all enhance the ability of disabled persons to interact with computers. For example, we are planning to allow the android phone to act as a motion controller for windows, so that a disabled person can interact with their pc without ever having to physically interact with the keyboard or mouse.
+
+ We hope this video inspired your curiosity, both in terms of what physically disabled persons have to go through and the
+ capabilities of modern tech to make their lives easier. Thank you!
