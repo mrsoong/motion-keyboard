bdaac2223287ef765235cd73a9101f44b20582d1
Mansoor Saqib <mansoor.saqib@mail.utoronto.ca>
2017-03-05 23:04:28 -0500

    Keys are highlighted if the cursor is above them

---
+++ MotionKey/app/src/main/java/com/example/motionkey/MotionKeyCursorObserver.java
@@ -0,0 +1,51 @@
+package com.example.motionkey;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Copyright (C) 2017 MotionKey
+ */
+
+public class MotionKeyCursorObserver {
+
+    MotionKeyKeyboardView mMotionKeyView;
+    List<MotionKeyInteractiveElement> mAllInteractiveElements = new ArrayList<MotionKeyInteractiveElement>();
+    private boolean mAllElementsAdded = false;
+    private int[] mCursorPosition;
+
+
+    public MotionKeyCursorObserver(MotionKeyKeyboardView motionkeykeyboardview) {
+        mMotionKeyView = motionkeykeyboardview;
+        findElements(mMotionKeyView.findViewById(R.id.keyboard_parent_layout));
+        mAllElementsAdded = true;
+    }
+
+
+    //recursively add all the button views to the list of interactive elements
+    public void findElements(View parent_view){
+        for (int i=0; i < ((ViewGroup)parent_view).getChildCount(); i++) {
+            View tmp_child = ((ViewGroup) parent_view).getChildAt(i);
+            if (tmp_child instanceof Button) {
+                mAllInteractiveElements.add(new MotionKeyInteractiveElement(tmp_child));
+            }
+            if (!(tmp_child instanceof Button || tmp_child instanceof TextView || tmp_child instanceof ImageView)) {
+                if (((ViewGroup) tmp_child).getChildCount() > 0) {
+                    findElements(tmp_child);
+                }
+            }
+        }
+    }
+
+    public void updateCursorPosition(int[] position) {
+        for (MotionKeyInteractiveElement element : mAllInteractiveElements) {
+            element.checkCursorHover(position);
+        }
+    }
+}
---
+++ MotionKey/app/src/main/java/com/example/motionkey/MotionKeyInteractiveElement.java
@@ -0,0 +1,54 @@
+package com.example.motionkey;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Copyright (C) 2017 MotionKey
+ * File Created on 3/4/17
+ */
+
+public class MotionKeyInteractiveElement {
+
+    private View mView;
+    private int mId;
+//    private int mLastColour;
+    private boolean mCursorHovering = false;
+    private int mHeight;
+    private int mWidth;
+    private int[] mPosition = new int[2];
+    private int mColour;
+
+    public MotionKeyInteractiveElement(View view) {
+        mView = view;
+        mId = view.getId();
+//        mLastColour = ((ColorDrawable)(view.getBackground())).getColor();
+        mHeight = view.getHeight();
+        mWidth = view.getWidth();
+        view.getLocationInWindow(mPosition);
+        mColour = ((ColorDrawable)view.getBackground()).getColor();
+//        Log.d("keyboard", "color: "+mColour);
+    }
+
+    public void checkCursorHover(int[] position) {
+        if (position[0] > mPosition[0] && position[0] < (mPosition[0] + mWidth)) {
+            if (position[1] > mPosition[1] && position[1] < (mPosition[1] + mHeight)) {
+                mCursorHovering = true;
+                mView.setBackgroundColor(Color.parseColor("#80ddff"));
+                return;
+            }
+        }
+//        if (mView.getTag() != null) {
+//            if (((String)mView.getTag()).equals("thedkey")) {
+//                Log.d("keyboard", "d: "+" x: "+mPosition[0] + " y: "+mPosition[1]);
+//                Log.d("keyboard", "cursor: "+" x: "+position[0] + " y: "+position[1]);
+//            }
+//        }
+
+        mCursorHovering = false;
+        mView.setBackgroundColor(mColour);
+    }
+}
