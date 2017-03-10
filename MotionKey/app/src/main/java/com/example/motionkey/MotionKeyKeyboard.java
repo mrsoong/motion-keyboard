cb6852b8125d5f6650c7faefa5164ad9b444be88
MarkFranciscus <MarkFranciscus@users.noreply.github.com>
2017-03-10 01:49:43 -0500

    Update MotionKeyKeyboard.java
    removed useless import

--- MotionKey/app/src/main/java/com/example/motionkey/MotionKeyKeyboard.java
+++ MotionKey/app/src/main/java/com/example/motionkey/MotionKeyKeyboard.java
@@ -15,7 +15,6 @@ import android.inputmethodservice.InputMethodService;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
